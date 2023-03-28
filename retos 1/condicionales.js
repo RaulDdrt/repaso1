5475093feecbac9d2f6e938e0606c055d5228494
RaulDdrt <rauldiazdrt@gmail.com>
2023-03-28 10:05:38 +0100

    segundo commit

--- retos 1/condicionales.js
+++ retos 1/condicionales.js
@@ -1,108 +1,68 @@
-function zodiac (day, month){
+// function zodiac(day, month) {
+//     if (day >= 21 && month == 3 || day <= 20 && month == 4) {
+//         return "Tu signo del zodiaco es Aries";
+//     }
+//     else if (day >= 21 && month == 7 || day <= 20 && month == 5) {
+//         return "Tu signo del zodiaco es Tauro";
+//     }
+//     else if (day >= 21 && month == 5 || day <= 20 && month == 6) {
+//         return "Tu signo del zodiaco es Geminis";
+//     }
+//     else if (day >= 21 && month == 6 || day <= 22 && month == 7) {
+//         return "Tu signo del zodiaco es Cancer";
+//     }
+//     else if (day >= 23 && month == 7 || day <= 23 && month == 8) {
+//         return "Tu signo del zodiaco es Leo";
+//     }
+//     else if (day >= 24 && month == 8 || day <= 22 && month == 9) {
+//         return "Tu signo del zodiaco es Virgo";
+//     }
+//     else if (day >= 23 && month == 9 || day <= 23 && month == 10) {
+//         return "Tu signo del zodiaco es Libra";
+//     }
+//     else if (day >= 24 && month == 10 || day <= 22 && month == 11) {
+//         return "Tu signo del zodiaco es Escorpio";
+//     }
+//     else if (day >= 23 && month == 11 || day <= 21 && month == 12) {
+//         return "Tu signo del zodiaco es Sagitario";
+//     }
+//     else if (day >= 22 && month == 12 || day <= 20 && month == 1) {
+//         return "Tu signo del zodiaco es Capricornio";
+//     }
+//     else if (day >= 21 && month == 1 || day <= 19 && month == 2) {
+//         return "Tu signo del zodiaco es Acuario";
+//     }
+//     else if (day >= 20 && month == 2 || day <= 20 && month == 2) {
+//         return "Tu signo del zodiaco es Piscis";
+//     }
+// }
+// function continent(country) {
+//     if (country == "España" || country == "Francia" || country == "Reino Unido" || country == "Belgica" || country == "Malta") {
+//         return "Europa";
+//     }
+//     if (country == "Zimbawee" || country == "Marruecos" || country == "Egipto" || country == "Congo" || country == "Madagascar") {
+//         return "Africa";
+//     }
+//     if (country == "Japon" || country == "China" || country == "Korea del sur" || country == "Korea del norte" || country == "Tailandia") {
+//         return "Asia";
+//     }
+//     if (country == "Venezuela" || country == "Argentina" || country == "Uruguay" || country == "Paraguay" || country == "Colombia") {
+//         return "Sudamerica";
+//     }
+//     if (country == "USA" || country == "Mexico" || country == "Canada") {
+//         return "Norte America";
+//     }
+//     if (country == "Australia" || country == "Nueva Zelanda") {
+//         return "Oceania";
+//     }
+// }
+// function isEven(number) {
+//     if (number % 2 == 0) {
+//         console.log("El numero es par");
+//     }
+//     else {
+//         console.log("El numero es impar");
+//     }
+// }
 
-    if (day >= 21 && month == "Marzo" || day <= 20 && month == "Abril"){
-
-        return "Tu signo del zodiaco es Aries"
-    }
-
-    else if (day >= 21 && month == "Abril" || day <= 20 && month == "Mayo"){
-
-        return "Tu signo del zodiaco es Tauro"
-    }
-
-    else if (day >= 21 && month == "Mayo" || day <= 20 && month == "Junio"){
-
-        return "Tu signo del zodiaco es Geminis"
-    }
-
-    else if (day >= 21 && month == "Junio" || day <= 22 && month == "Julio"){
-
-        return "Tu signo del zodiaco es Cancer"
-    }
-
-    else if (day >= 23 && month == "Julio" || day <= 23 && month == "Agosto"){
-
-        return "Tu signo del zodiaco es Leo"
-    }
-
-    else if (day >= 24 && month == "Agosto" || day <= 22 && month == "Septiembre"){
-
-        return "Tu signo del zodiaco es Virgo"
-    }
-
-    else if (day >= 23 && month == "Septiembre" || day <= 23 && month == "Octubre"){
-
-        return "Tu signo del zodiaco es Libra"
-    }
-
-    else if (day >= 24 && month == "Octubre" || day <= 22 && month == "Noviembre"){
-
-        return "Tu signo del zodiaco es Escorpio"
-    }
-
-    else if (day >= 23 && month == "Noviembre" || day <= 21 && month == "Diciembre"){
-
-        return "Tu signo del zodiaco es Sagitario"
-    }
-
-    else if (day >= 22 && month == "Diciembre" || day <= 20 && month == "Enero"){
-
-        return "Tu signo del zodiaco es Capricornio"
-    }
-
-    else if (day >= 21 && month == "Enero" || day <= 19 && month == "Febrero"){
-
-        return "Tu signo del zodiaco es Acuario"
-    }
-
-    else if (day >= 20 && month == "Febrero" || day <= 20 && month == "Marzo"){
-
-        return "Tu signo del zodiaco es Piscis"
-    }
-
-}
-
-// let zodiaco = zodiac(28,"Agosto")
-
-// console.log(zodiaco)
-
-function continent(country){
-
-    if( country == "España" || country == "Francia" || country == "Reino Unido" || country == "Belgica" || country == "Malta"){
-
-        return "Europa"
-    }
-
-    if( country == "Zimbawee" || country == "Marruecos" || country == "Egipto" || country == "Congo" || country == "Madagascar"){
-
-        return "Africa"
-    }
-
-    if( country == "Japon" || country == "China" || country == "Korea del sur" || country == "Korea del norte" || country == "Tailandia"){
-
-        return "Asia"
-    }
-
-    if( country == "Venezuela" || country == "Argentina" || country == "Uruguay" || country == "Paraguay" || country == "Colombia"){
-
-        return "Sudamerica"
-    }
-
-    if( country == "USA" || country == "Mexico" || country == "Canada"){
-
-        return "Norte America"
-    }
-
-    if( country == "Australia" || country == "Nueva Zelanda"){
-
-        return "Oceania"
-    }
-
-
-
-}
-
-// let continente = continent("Australia")
-
-// console.log(continente)
-
+// isEven(3)
---
+++ retos 1/condicionales.ts
@@ -0,0 +1,113 @@
+function zodiac (day:number, month:number){
+
+    if (day >= 21 && month == 3 || day <= 20 && month == 4){
+
+        return "Tu signo del zodiaco es Aries"
+    }
+
+    else if (day >= 21 && month == 7 || day <= 20 && month == 5){
+
+        return "Tu signo del zodiaco es Tauro"
+    }
+
+    else if (day >= 21 && month == 5 || day <= 20 && month == 6){
+
+        return "Tu signo del zodiaco es Geminis"
+    }
+
+    else if (day >= 21 && month == 6 || day <= 22 && month == 7){
+
+        return "Tu signo del zodiaco es Cancer"
+    }
+
+    else if (day >= 23 && month == 7 || day <= 23 && month == 8){
+
+        return "Tu signo del zodiaco es Leo"
+    }
+
+    else if (day >= 24 && month == 8 || day <= 22 && month == 9){
+
+        return "Tu signo del zodiaco es Virgo"
+    }
+
+    else if (day >= 23 && month == 9 || day <= 23 && month == 10){
+
+        return "Tu signo del zodiaco es Libra"
+    }
+
+    else if (day >= 24 && month == 10 || day <= 22 && month == 11){
+
+        return "Tu signo del zodiaco es Escorpio"
+    }
+
+    else if (day >= 23 && month == 11 || day <= 21 && month == 12){
+
+        return "Tu signo del zodiaco es Sagitario"
+    }
+
+    else if (day >= 22 && month == 12 || day <= 20 && month == 1){
+
+        return "Tu signo del zodiaco es Capricornio"
+    }
+
+    else if (day >= 21 && month == 1 || day <= 19 && month == 2){
+
+        return "Tu signo del zodiaco es Acuario"
+    }
+
+    else if (day >= 20 && month == 2 || day <= 20 && month == 2){
+
+        return "Tu signo del zodiaco es Piscis"
+    }
+
+}
+
+function continent(country:string){
+
+    if( country == "España" || country == "Francia" || country == "Reino Unido" || country == "Belgica" || country == "Malta"){
+
+        return "Europa"
+    }
+
+    if( country == "Zimbawee" || country == "Marruecos" || country == "Egipto" || country == "Congo" || country == "Madagascar"){
+
+        return "Africa"
+    }
+
+    if( country == "Japon" || country == "China" || country == "Korea del sur" || country == "Korea del norte" || country == "Tailandia"){
+
+        return "Asia"
+    }
+
+    if( country == "Venezuela" || country == "Argentina" || country == "Uruguay" || country == "Paraguay" || country == "Colombia"){
+
+        return "Sudamerica"
+    }
+
+    if( country == "USA" || country == "Mexico" || country == "Canada"){
+
+        return "Norte America"
+    }
+
+    if( country == "Australia" || country == "Nueva Zelanda"){
+
+        return "Oceania"
+    }
+
+
+
+}
+
+function isEven(number:number){
+
+    if( number % 2 == 0){
+
+        console.log("El numero es par")
+    }
+
+    else{
+
+        console.log("El numero es impar")
+    }
+
+}
